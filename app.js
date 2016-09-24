84285dd00c18a7d8d51073f3c8c24b51edf039c6
Djane Rey Mabelin <thedjaney@gmail.com>
2016-09-25 01:04:00 +0800

    clean up

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var game = require('./src/main.js');
+var game = require('.');
 
 app.use('/tennis', game.router);
 
